0aeb64522ec752b13c3fa863d4c92d596a8b9519
Harshal Bhendare <104885882+Harshu2110@users.noreply.github.com>
2024-10-16 11:38:59 +0530

    Add files via upload

---
+++ Bankcruptcy/Deployment/bankcurptcy.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 25 11:10:52 2024
+
+@author: Harshal
+"""
+
+import streamlit as st
+
+st.title('bankruptcy')
+
+st.markdown('This model gives the prediction of bankruptcy')
